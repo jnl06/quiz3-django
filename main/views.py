15be5866c3887152eb9f5c04a89165d92a339421
Jonel <jonelpronton06@gmail.com>
2026-09-23 19:04:25 +0800

    Add files via upload

---
+++ main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+def student_list(request):
+    student = [
+        {"name": "Jomar Jorquia", "age": 18, "course": "BSIT"},
+        {"name": "Zedrick Lacuesta", "age": 19, "course": "BSA"},
+        {"name": "Mark Anthony Llanora", "age": 20, "course": "BSE"},
+        {"name": "Jhonas San Agustin", "age": 21, "course": "BSIT"},
+        {"name": "Mark Vincent De Guzman", "age": 22, "course": "BSAIS"},
+        {"name": "Rafael Metran", "age": 23, "course": "BSE"},
+        {"name": "Neftaly Careg", "age": 24, "course": "BSA"},
+        {"name": "Ceejay Petalvero", "age": 25, "course": "BSIT"},
+        {"name": "Carl Chua", "age": 26, "course": "BSA"}
+    ]
+    return render(request, "main/home.html", {"student": student})
